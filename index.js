9ea4a5597aca83418ad29900710a7097ada50c56
Tishhshaw <Tish.shaw@yahoo.com>
2022-03-04 14:31:29 -0500

    add cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 app.set('port', process.env.PORT || 5000);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get('/', (req, res) => {
 	res.redirect('/api/questions');
